432e81113b84d3cd66419ff5ace7e033511a69b9
SharanSapkota <46062039+SharanSapkota@users.noreply.github.com>
2019-10-14 08:32:59 +0545

    Add files via upload

---
+++ SampleExercise.java
@@ -0,0 +1,24 @@
+public class SampleExercise {
+    public static void main(String[] args) {
+        checknumber(0);
+
+    }
+    public static void checknumber(int a){
+        if (a>0){
+            System.out.println("positive");
+
+        }
+        else if (a<0){
+                System.out.println("negative");
+            }
+            System.out.println("zero");
+
+
+
+
+    }
+
+
+
+
+}
